b055ba3bcf47a4dd67081e9cd74c33245eadf2ba
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-30 23:48:38 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,12 +6,11 @@ class EmailParser
   
   attr_reader :emails
   
-  #def initialize
-    #@emails= emails
-  #end
+  def initialize
+    @emails= emails
+  end
   
   def self.parse(emails)
-    @emails = emails
     @emails.split.map {|email| email.split(",")}.flatten.uniq
   end
     
